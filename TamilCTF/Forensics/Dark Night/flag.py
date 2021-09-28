ef8c6e36b9d08edc42530bb8272268b6890a1889
0xM4hm0ud <80924519+0xM4hm0ud@users.noreply.github.com>
2021-09-28 21:08:03 +0200

    Create flag.py

---
+++ TamilCTF/Forensics/Dark Night/flag.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+a = [84, 97, 109, 105, 108, 67, 84, 70, 123, 49, 95, 108, 48, 118, 51, 95, 98, 98, 52, 52, 116, 116, 95, 109, 97, 110, 125] #taken from the pdf
+b = ''
+
+for i in a:
+        c = chr(i)
+        b += c
+print(b)
+
+#TamilCTF{1_l0v3_bb44tt_man}
