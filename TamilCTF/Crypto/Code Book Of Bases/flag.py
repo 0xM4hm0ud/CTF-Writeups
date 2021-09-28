b69d94a071a4716fa8f266b2db073ef33c7b7168
0xM4hm0ud <80924519+0xM4hm0ud@users.noreply.github.com>
2021-09-28 20:57:31 +0200

    Create flag.py

---
+++ TamilCTF/Crypto/Code Book Of Bases/flag.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import base64
+from Crypto.Cipher import AES
+
+key = b'keytamilctf2021!' # To get the key, you needed to encode it to base64, then decode from binary
+ct = 'oPgiWmZzdeMhyA80iS9c6la2TlIuIJ1HFRAEvH+8zgo='
+base = base64.b64decode(ct)
+
+decipher = AES.new(key, AES.MODE_ECB)
+pt = decipher.decrypt(base)
+print(pt)
